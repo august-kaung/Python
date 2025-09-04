f0167e23a376b6266d9270b150adcd390702f49b
Kaung Htet <kaunghtetcoding22@gmail.com>
2025-09-04 07:31:35 +0630

    enum

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI,HTTPException
-
+from enum import Enum
 app = FastAPI()
 dummy_items = [
     {"item_id": 1, "name": "Laptop", "price": 1200.00},
     {"item_id": 2, "name": "Mouse", "price": 25.50},
-    {"item_id": 3, "name": "Keyboard", "price": 75.00},
+    {"item_id": 2, "name": "Keyboard", "price": 75.00},
     {"item_id": 4, "name": "Monitor", "price": 300.00},
 ]
+class EnumClass(Enum):
+    Laptop="laptop"
+    Mouse="mouse"
+    Keyboard="keyboard"
+    Monitor="monitor"
+
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "223"}
@@ -18,8 +24,18 @@ async def about() -> str:
 async def allBands() -> list[dict]:
     return dummy_items
 @app.get("/bands/{band_id}")
-async def band(band_id : int) -> dict:
-    band= next((item for item in dummy_items if item["item_id"] == band_id), None)
+async def band(band_id : int) -> list[dict]:
+    # band= next((item for item in dummy_items if item["item_id"] == band_id), None) #For one only
+    band = [item for item in dummy_items if item["item_id"] == band_id] #For all
+    if(band!=None):
+        return band
+    else:
+        raise HTTPException(status_code=404, detail="Band not found")
+    
+@app.get("/genere/{genere}")
+async def genere(genere : EnumClass) -> list[dict]:
+    # band= next((item for item in dummy_items if item["item_id"] == band_id), None) #For one only
+    band = [item for item in dummy_items if item["name"].lower() == genere.value.lower()] #For all
     if(band!=None):
         return band
     else:
